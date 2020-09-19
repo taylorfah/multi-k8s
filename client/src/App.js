0ca9bf1447b6968420bd5bbc67b99ed3c0bb08c6
Taylor Fah <taylorfah@Taylors-MacBook-Pro.local>
2020-09-19 15:55:50 -0700

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Working on kubernetes!!</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <Link to="/otherpage">test</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
